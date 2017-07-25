196bd3a595f82ab4f73b9d4ff407a3506758bcb8
Elias Ojala <theel0ja@users.noreply.github.com>
2017-07-25 13:53:32 +0300

    New themes for Xamarin

--- Themes/xamarin-android.php
+++ Themes/xamarin-android.php
@@ -1,7 +1,4 @@
 <?php
-    //error_reporting(E_ALL & ~E_NOTICE); # Notice: Use of undefined constant   - assumed ' ' in /home/theel0ja/dev/Themes/default.php on line 21 & 33
-    # just for debug ^
-
     function FoodList($data) {
         foreach($data as $food) {
             echo $food . "<br />";
@@ -13,8 +10,7 @@
 <html>
     <head>
         <meta charset="UTF-8" />
-        <title><?php echo _("Week's School Meal"); ?> - <?php echo $data["name"]; ?></title>
-        
+
         <style>
             /* W3.CSS 2.89 Jan 2017 by Jan Egil and Borge Refsnes */
             /* Modified for Viikon kouluruoka "Plain" theme by Elias Ojala (github.com/theel0ja) */
@@ -24,10 +20,14 @@
             h1{font-size:36px}h2{font-size:30px}h3{font-size:24px}h4{font-size:20px}h5{font-size:18px}h6{font-size:16px}
             h1,h2,h3,h4,h5,h6{font-weight:400;margin:10px 0}
             h1 a,h2 a,h3 a,h4 a,h5 a,h6 a{font-weight:inherit}
+
+            html {
+                background-color: #FAFAFA;
+            }
         </style>
     </head>
     <body>
-        <h1><?php echo $data["name"]; ?></h1>
+        <!--<h1><?php echo $data["name"]; ?></h1>-->
         <?php
             if( count($data["menu"]) == 0 ) {
                 echo _("No days to show.");
--- Themes/xamarin-other.php
+++ Themes/xamarin-other.php
@@ -1,7 +1,4 @@
 <?php
-    //error_reporting(E_ALL & ~E_NOTICE); # Notice: Use of undefined constant   - assumed ' ' in /home/theel0ja/dev/Themes/default.php on line 21 & 33
-    # just for debug ^
-
     function FoodList($data) {
         foreach($data as $food) {
             echo $food . "<br />";
@@ -13,8 +10,7 @@
 <html>
     <head>
         <meta charset="UTF-8" />
-        <title><?php echo _("Week's School Meal"); ?> - <?php echo $data["name"]; ?></title>
-        
+
         <style>
             /* W3.CSS 2.89 Jan 2017 by Jan Egil and Borge Refsnes */
             /* Modified for Viikon kouluruoka "Plain" theme by Elias Ojala (github.com/theel0ja) */
@@ -27,7 +23,7 @@
         </style>
     </head>
     <body>
-        <h1><?php echo $data["name"]; ?></h1>
+        <!--<h1><?php echo $data["name"]; ?></h1>-->
         <?php
             if( count($data["menu"]) == 0 ) {
                 echo _("No days to show.");
